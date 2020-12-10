147864dd4ce4909f3c9a7e3d027ad78b2ab197b3
lvivduncan <lvivduncan@gmail.com>
2020-12-10 23:41:58 +0200

    middle

---
+++ js/levus-pagination.js
@@ -0,0 +1,54 @@
+// 10-12-2020
+function levusPagination(number){
+
+    // обгортка
+    const wrap = document.querySelector('.levus-pagination');
+
+    // елементи
+    const el = wrap.querySelectorAll('.levus-item');
+    
+    // навігація
+    const nav = wrap.querySelector('.levus-pagination-nav');
+
+    // кількість сторінок
+    const pages = Math.round(el.length / number);
+
+    // виводимо сторінки
+    for(let i = 1; i <= pages; i++){
+        nav.innerHTML += `<span>${i}</span>`;
+    }
+
+    // 
+    let counter = 0;
+
+    el.forEach((item,i) => {
+        // усі ховаємо
+        item.classList.add('hide');
+
+        // показуємо тільки перші "н" 
+        if(i<number){
+            item.classList.remove('hide');
+        }
+    });
+
+    nav.addEventListener('click', e => {
+        if(e.target.tagName == 'SPAN'){
+            const n = e.target.textContent;
+
+            el.forEach((item,i) => {
+                // усі ховаємо
+                item.classList.add('hide');
+        
+                if(i<number*n){
+                    item.classList.remove('hide');
+                }
+            });
+
+
+        }
+    });
+
+    
+}
+
+levusPagination(8);
